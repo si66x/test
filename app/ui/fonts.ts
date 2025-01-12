bd4fdf7f219f4f5aec7fee6d84c9455bc10e0c2c
milano <khalifamilano.lm@gmail.com>
2025-01-12 16:26:33 +0700

    give it a try

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,5 +2,5 @@ import {Inter, Lusitana} from 'next/font/google';
 
 export const inter = Inter({subsets:['latin']})
 
-export const lusitana = Lusitana({weight:"700", style: "normal"})
+export const lusitana = Lusitana({weight:"700", style: "normal", subsets: ['latin']})
 
